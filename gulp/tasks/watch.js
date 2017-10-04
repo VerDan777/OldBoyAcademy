816e02c930e1a49a6792c2873912ecdbd38dd836
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-10-04 16:47:57 +0200

    updating workflow

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,28 +1,28 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
-const BrowserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 
-gulp.task('watch', ()=> {
-    BrowserSync.init({
+gulp.task('watch', () => {
+    browserSync.init({
         server: {
             baseDir: 'dist'
         }
+    });
+
+    watch('./src/**/*.pug', () => {
+        gulp.start('pugChanged');
+    })
+
+    watch('./src/**/*.scss', () => {
+        gulp.start('cssInject');
     })
 });
 
-watch('./src/**/*.pug',()=> {
-    gulp.start('PugChanged');
-});
-
-watch('./src/**/*.scss',()=> {
-    gulp.start('CssInject');
-});
-
-gulp.task('PugChanged',['pugRender'],()=> {
+gulp.task('pugChanged', ['pugRender'], () => {
     browserSync.reload();
 });
 
-gulp.task('CssInject',['style'],()=> {
-    gulp.src('./src/dist/styles.css')
-    .pipe(BrowserSync.stream());
+gulp.task('cssInject', ['styles'], () => {
+    gulp.src('./dist/styles.css')
+    .pipe(browserSync.stream());
 });
