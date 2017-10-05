3c8b0660fca286fbc094ba64412c32d97da997e4
Verdan777 <vereshhagin98@mail.ru>
2017-10-05 10:36:44 +0200

    some fixes

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch',function() {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+watch("./src/**/*.pug",function() {
+    gulp.start("pugChanged");
+});
+watch("./src/**/*.scss",function() {
+    gulp.start('cssInject');
+})
+
+});
+
+gulp.task("pugChanged",["pugRender"],function() {
+    browserSync.reload();
+});
+gulp.task("cssInject",["styles"], function() {
+    gulp.src("./dist/styles.css")
+    .pipe(browserSync.stream());
+})
